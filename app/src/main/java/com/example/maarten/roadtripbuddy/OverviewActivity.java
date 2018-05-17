a8bbaf106a3f27ccaf36f250bbb544cefc1da2ef
maartenpwn <maartenstolk72@gmail.com>
2018-05-17 18:35:20 +0200

    Added warning on low battery level

--- app/src/main/java/com/example/maarten/roadtripbuddy/OverviewActivity.java
+++ app/src/main/java/com/example/maarten/roadtripbuddy/OverviewActivity.java
@@ -1,7 +1,9 @@
 package com.example.maarten.roadtripbuddy;
 
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,6 +19,8 @@ public class OverviewActivity extends AppCompatActivity {
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final String TAG = "poep";
 
+    private BroadcastReceiver mReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +29,20 @@ public class OverviewActivity extends AppCompatActivity {
         if(isServicesOK()){
             init();
         }
+
+        mReceiver = new ReceiveBroadcast();
+    }
+
+    /* For broadcast receiver */
+    @Override
+    protected void onStart() {
+        registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+        unregisterReceiver(mReceiver);
+        super.onStop();
     }
 
     private void init(){
---
+++ app/src/main/java/com/example/maarten/roadtripbuddy/ReceiveBroadcast.java
@@ -0,0 +1,50 @@
+package com.example.maarten.roadtripbuddy;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class ReceiveBroadcast extends BroadcastReceiver {
+
+    private final static String BATTERY_LEVEL = "level";
+    private static final String TAG = "poep";
+
+    public ReceiveBroadcast() {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        int level = intent.getIntExtra(BATTERY_LEVEL, 0);
+
+        if(level < 26){
+            Log.d(TAG, "onReceive: low battery!" + level);
+            showNotification(context);
+        }
+    }
+
+    private void showNotification(Context context) {
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, OverviewActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.city)
+                        .setContentTitle("Battery low!")
+                        .setContentText("Charge your phone for your city activities!");
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
+
+    }
+
+}
